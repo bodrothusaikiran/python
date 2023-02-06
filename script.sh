8141fd7cbb78cd083d20ee31acba7990e1a04564
Saikiran@456 <112848498+bodrothusaikiran@users.noreply.github.com>
2023-02-06 14:48:20 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,4 +2,8 @@ git clone https://github.com/bodrothusaikiran/Agri.git
 cd Agri
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
-git clone 
+
+git clone https://github.com/bodrothusaikiran/Fuel-Consumption-Rating.git 
+cd Fuel-Consumption-Rating
+pip3 install -r requirements.txt
+screen -m -d python3 app.py
